63f8e0a476f0d90f73e932730c4e2b37238d27d5
Danish Ayub Khan <34853491+danissh20@users.noreply.github.com>
2018-03-01 00:11:15 +0530

    Create export_to_csv.php

---
+++ Railcon/export_to_csv.php
@@ -0,0 +1,22 @@
+<?php
+// Connection 
+$conn=mysql_connect('localhost','root','');
+//database name
+$db=mysql_select_db('railcon',$conn);
+
+$filename = "RailwayConcession.xls"; // File Name
+// Download file
+header("Content-Disposition: attachment; filename=\"$filename\"");
+header("Content-Type: application/vnd.ms-excel");
+$user_query = mysql_query('select * from student');
+// Write data to file
+$flag = false;
+while ($row = mysql_fetch_assoc($user_query)) {
+    if (!$flag) {
+        // display field/column names as first row
+        echo implode("\t", array_keys($row)) . "\r\n";
+        $flag = true;
+    }
+    echo implode("\t", array_values($row)) . "\r\n";
+}
+?>
