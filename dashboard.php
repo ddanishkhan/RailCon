6ba933fbb3248a03eed073b48d66ec113a430bdd
PsychoCoder <mohdshibli27@gmail.com>
2018-07-16 08:51:22 +0530

    changes made to respective files for ajax

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	include('database_connection.php');
 	$_SESSION['dashboard'] = true;
-	
+
 	$size = 10;
 	if(isset($_GET['page'])){
 		$start = $_GET['page']*$size;
@@ -12,7 +12,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 		$start=0;
 	}
 
-	$sql_display = "SELECT id, fullname, gender,DOB, source, destination, passno,DATE_FORMAT(pass_end, '%d/%m/%y') AS pass_end,voucher,season,classof, duration, verified,img_loc, DATE_FORMAT(dateofentry, '%d/%m/%Y') AS date 
+	$sql_display = "SELECT id, fullname, gender,DOB, source, destination, passno,DATE_FORMAT(pass_end, '%d/%m/%y') AS pass_end,voucher,season,classof, duration, verified,img_loc, DATE_FORMAT(dateofentry, '%d/%m/%Y') AS date
 	FROM student LIMIT $start, $size";
 	$result = $db->query($sql_display);
 	//stored result object of query here
@@ -46,8 +46,9 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 	<link rel='stylesheet' type='text/css' href='modal.css'>
+	<script type="text/JavaScript" src="js/ajax.js"></script>
 	<style>
-	
+
 	</style>
   </head>
   <body>
@@ -69,15 +70,15 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
                 <!-- Navbar Brand --><a href="#" class="navbar-brand d-none d-sm-inline-block">
                   <div class="brand-text d-none d-lg-inline-block"><span>RailCon </span><strong>Dashboard</strong></div>
                   <div class="brand-text d-none d-sm-inline-block d-lg-none"><strong>Railway Concession</strong></div></a>
-                
+
               </div>
               <!-- Navbar Menu -->
               <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-			  
+
                 <!--Download as Excel File-->
-				<li class="nav-link"><a href="export_to_csv.php"><i class="fa fa-file-text"></i><span class="d-none d-sm-inline">Download  
+				<li class="nav-link"><a href="export_to_csv.php"><i class="fa fa-file-text"></i><span class="d-none d-sm-inline">Download
 				</span></a></li>
-				
+
 				<!-- Search-->
                 <li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
 
@@ -88,7 +89,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
           </div>
         </nav>
       </header>
-      <div class="page-content align-items-stretch"> 
+      <div class="page-content align-items-stretch">
 
         <div class="content-inner" style="width:100%">
           <!-- Breadcrumb-->
@@ -98,16 +99,16 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
               <li class="breadcrumb-item active">Dashboard - All Entries  </li>
             </ul>
           </div>
-          <section class="tables">   
+          <section class="tables">
             <div class="container-fluid">
               <div class="row">
                 <div class="col-lg-12">
                   <div class="card">
                     <div class="card-header d-flex align-items-center">
-	
+
 <!--Pagination -->
 <nav aria-label="...">
-<ul class="pagination">				
+<ul class="pagination">
 </html>
 <?php
 	//page number
@@ -124,11 +125,11 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 		}
 ?>
 
-<html>				
+<html>
                       <!--<h3 class="h4">Compact Table</h3>-->
                     </div>
                     <div class="card-body">
-                      <div class="table-responsive">   
+                      <div class="table-responsive">
                         <table class="table table-striped table-sm">
                           <thead>
                             <tr>
@@ -139,17 +140,17 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 							  <th>Source</th>
 							  <th>Destination</th>
 							  <th>Passno</th>
-							  <th>Class</th> 
-							  <th>Duration</th> 
-							  <th>DateOfEntry</th> 
-							  <th>Status</th> 
-							  <th>ID Card</th> 
-							  <th>Issue</th> 
-							  <th>Remarks</th> 
+							  <th>Class</th>
+							  <th>Duration</th>
+							  <th>DateOfEntry</th>
+							  <th>Status</th>
+							  <th>ID Card</th>
+							  <th>Issue</th>
+							  <th>Remarks</th>
                             </tr>
                           </thead>
 						  <tbody>
-							
+
 </html>
 
 <?php
@@ -163,7 +164,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 			if( $row['gender']=='1' )
 				echo "Female";
 			else
-				echo "Male";	
+				echo "Male";
 		echo "</td><td>";
 				$diff = date_diff(date_create(), date_create($row['DOB']) );
 				echo $diff->format("%Y Yrs <br/> %M Mnth");
@@ -182,7 +183,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 			echo $row['duration'];
 		echo "</td><td>";
 		    echo $row['date'];
-		echo "</td><td>";	
+		echo "</td><td>";
 			if($row['verified']=="1" )
 				echo "Issued";
 			else
@@ -193,7 +194,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	<!-- The Modal -->
 	<!-- Be very careful editing this -->
 	<div id='myModal".$idd."' class='modal'>
-	<span class='close".$idd."' 
+	<span class='close".$idd."'
 	style='position: absolute;
     top: 15px;
     right: 35px;
@@ -204,7 +205,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	>&times;</span>
 	<img class='modal-content' id='img1".$idd."'>
 	</div>
-	
+
 	<script>
 	// Get the modal
 	var modal = document.getElementById('myModal".$idd."');
@@ -218,16 +219,18 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName('close".$idd."')[0];
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() { 
+	span.onclick = function() {
 		modal.style.display = 'none';
 	}
 	</script>
 			";
 		echo "</td><td>";
 
-			echo '<form action="update.php" method="POST">
+			echo '
+			<input onclick="issue('.$idd.')" type = "submit" class="bg-green" name= "verify_it" value="Issue">
+			<form action="update.php" method="POST">
 			<input type="hidden" name = "id" value = '.$idd .'>
-			<input type = "submit" class="bg-green" name= "verify_it" value="Issue"><br/>
+			<br/>
 			<input type = "submit" class="bg-red" name= "cancel_verify" value="Not Issue">
 			</form>';
 		echo "</td><td>";
@@ -241,8 +244,8 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	}
 	}//end if
 ?>
-						 
-<html>						  
+
+<html>
                             <tr>
                               <th scope="row">-</th>
                               <td>-</td>
@@ -264,7 +267,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
                       </div>
                     </div>
                   </div>
-               
+
               </div>
             </div>
           </section>
@@ -293,7 +296,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
   </body>
 </html>
 
-<?php	
+<?php
 }
 else{
 	header("Refresh:1; url=login.html");
