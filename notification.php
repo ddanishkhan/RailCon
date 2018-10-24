f8bd28e921b12295401c601dd6a7a4f7f68c741b
Danish Ayub Khan <danissh20@gmail.com>
2018-10-24 19:32:25 +0530

    Removed Redundant Code
    Improvised Same Code Loading in comomn code
    Added Notification to the admin incase the form is closed for students in notificationFormClosed

--- notification.php
+++ notification.php
@@ -3,6 +3,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
+<?php
+
+?>
 <script>
     $("document").ready( function () {
 		
@@ -22,6 +25,9 @@
 		});
     }); 
 </script>
+<?php
+
+?>
 
 <?php
 if(isset($_GET['updated'])){
