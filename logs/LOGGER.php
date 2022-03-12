b3ed6dfb5089cfe6db62351c5a26ffc244dbdc84
Danish Ayub Khan <danissh20@gmail.com>
2022-03-12 20:36:49 +0530

    Minor updates

--- logs/LOGGER.php
+++ logs/LOGGER.php
@@ -7,5 +7,11 @@ class LOGGER
         $calledFromFile = basename($_SERVER["REQUEST_URI"]);
         error_log("\n[". date("jS F Y h:i:s A") ."] [".$severity."]" . "[$calledFromFile]" . "[".$message."]", 3, "logs/logs.log");
     }
+    
+    public static function logWithPath($severity, $message, $logPath)
+    {
+        $calledFromFile = basename($_SERVER["REQUEST_URI"]);
+        error_log("\n[". date("jS F Y h:i:s A") ."] [".$severity."]" . "[$calledFromFile]" . "[".$message."]", 3, $logPath);
+    }
 }
 ?>
